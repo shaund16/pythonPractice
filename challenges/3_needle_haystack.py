3a3796920aca0967faacdd2b38e90f071a269398
Shaun <s-purslow@hotmail.com>
2022-01-31 21:33:01 -0700

    complete kata

---
+++ challenges/3_needle_haystack.py
@@ -0,0 +1,5 @@
+def find_needle(haystack):
+       return 'found the needle at position ' + str(haystack.index('needle'))
+    
+print(find_needle(['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]))
+print(find_needle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]))
