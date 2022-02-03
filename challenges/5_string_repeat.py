a5f18f94c7a673c28aaaac10b66063f02f3d2a43
Shaun <s-purslow@hotmail.com>
2022-02-02 21:04:58 -0700

    complete kata

---
+++ challenges/5_string_repeat.py
@@ -0,0 +1,11 @@
+def repeat_str(repeat, string):
+  return repeat * string
+  
+  # solution = ""
+  #   for i in range(repeat):
+  #       solution += string
+  #   return solution
+  
+  
+print(repeat_str(4, 'a'))
+print(repeat_str(3, 'hello'))
