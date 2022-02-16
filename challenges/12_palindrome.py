c425a20ab8f9c8802ae2af887cd60bacea7f3c74
Shaun <s-purslow@hotmail.com>
2022-02-16 13:26:23 -0700

    complete kata

---
+++ challenges/12_palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+ return s.lower() == s[::-1].lower()
+
+
+print(is_palindrome('a'))
+print(is_palindrome('aba'))
+print(is_palindrome('Abba'))
+print(is_palindrome('malam'))
+print(is_palindrome('walter'))
