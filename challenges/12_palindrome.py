f3a9bcf3ef04cb765292cee539abb30335bda8be
Shaun <s-purslow@hotmail.com>
2022-03-20 20:30:11 -0600

    format

--- challenges/12_palindrome.py
+++ challenges/12_palindrome.py
@@ -1,9 +1,9 @@
 def is_palindrome(s):
- return s.lower() == s[::-1].lower()
+    return s.lower() == s[::-1].lower()
 
 
-print(is_palindrome('a'))
-print(is_palindrome('aba'))
-print(is_palindrome('Abba'))
-print(is_palindrome('malam'))
-print(is_palindrome('walter'))
+print(is_palindrome("a"))
+print(is_palindrome("aba"))
+print(is_palindrome("Abba"))
+print(is_palindrome("malam"))
+print(is_palindrome("walter"))
